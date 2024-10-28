0e938dc0dc950b9f3fd28a9eb6593ef75a002e6f
Micaela-Boler <bolermica@gmail.com>
2024-10-28 19:38:36 -0300

    Jugador puede cambiar de direccion

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Transform center;
+    [SerializeField] float radius;
+    [SerializeField] float speed;
+    [SerializeField] float angle;
+    private bool direction;
+
+
+
+    void Update()
+    {
+        circularMovement();
+        ChangeDirection();
+    }
+
+
+
+    private void circularMovement()
+    {
+        float x = center.position.x + Mathf.Cos(angle) * radius;
+        float y = center.position.y + Mathf.Sin(angle) * radius;
+        float z = center.position.z;
+
+
+        transform.position = (new Vector3(x, y, z));
+
+
+        switch (direction)
+        {
+            case true:
+                angle -= speed + Time.deltaTime;
+                break;
+
+            case false:
+                angle += speed + Time.deltaTime;
+                break;
+        }  
+    }
+
+
+    private void ChangeDirection()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            direction = !direction;
+        }
+    }
+}

21e6d67c7305d3bd9191207c8f3f073c8d04af37
Micaela-Boler <bolermica@gmail.com>
2024-11-10 16:05:50 -0300

    power up funciona
    se necesita limpiar el código

--- Assets/Scripts/SpeedPowerUp.cs
+++ Assets/Scripts/SpeedPowerUp.cs
@@ -7,10 +7,14 @@ public class SpeedPowerUp : MonoBehaviour
 {
     [SerializeField] Slider powerUpLoad;
     [SerializeField] [Range(0.001f, 0.004f)] float speedBoost;
+    private float normalSpeed;
 
 
     void Start()
     {
+        normalSpeed = gameObject.GetComponent<PlayerMovement>().speed;
+
+
         powerUpLoad.maxValue = 100;
         powerUpLoad.minValue = 0;
 
@@ -33,10 +37,17 @@ public class SpeedPowerUp : MonoBehaviour
             else
             {
                 gameObject.GetComponent<PlayerHealth>().canTakeDamage = true;
-                gameObject.GetComponent<PlayerMovement>().speed = 0.001f;
-                powerUpLoad.value += Time.deltaTime * 100; //HACER QUE NO SE PUEDA RECARGAR MIENTRAS SE ESTA USANDO LA HABILIDAD
+                gameObject.GetComponent<PlayerMovement>().speed = normalSpeed;
+                //powerUpLoad.value += Time.deltaTime * 100; //HACER QUE NO SE PUEDA RECARGAR MIENTRAS SE ESTA USANDO LA HABILIDAD
             }    
         }
+        else
+        {
+            gameObject.GetComponent<PlayerHealth>().canTakeDamage = true;
+            gameObject.GetComponent<PlayerMovement>().speed = normalSpeed;
+            powerUpLoad.value += Time.deltaTime * 100;
+        }
+        
 
         //CUANDO SE PRESIONA EL CAMBIO DE DIRECCION, SE ACTIVA EL POWER UP (SE USA EL CARGADOR CUANDO SE MANTIENE LA FLECHA)
     }
